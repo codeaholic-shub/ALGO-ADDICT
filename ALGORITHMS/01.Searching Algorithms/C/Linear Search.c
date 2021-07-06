2580830c422427739b6e2ed3a4edee494aac001b
Yash Raj Saxena <yashrajsaxena@Yashs-MacBook-Air.local>
2021-07-06 23:55:13 +0530

    change according to review  Linear Search code in c language

--- ALGORITHMS/01.Searching Algorithms/C/Linear Search.c
+++ ALGORITHMS/01.Searching Algorithms/C/Linear Search.c
@@ -1,30 +1,40 @@
 // Linear Search Algorithm
 #include <stdio.h>
  
-int Linear_search(int arr[], int size_of_array, int number_for_searching )
-{
-    int i;
-    for (i = 0; i < size_of_array; i++){
-        if (arr[i] == number_for_searching){
-            return i;
+void main()
+{  
+    // make two variable for input first "size_of_array" for the size of the array and 
+    // second "number_for_searching" for that number we have to find.
+    // result is used for storing index. 
+
+
+    //input formate
+    // size_of_array, number_for_searching
+    // Elements of the array
+    // example ==>
+    // 10 25
+    // 12 34 72 45 82 25 91 100 65 123
+    
+    int size_of_array, number_for_searching, result = -1;
+ 
+    scanf("%d %d", &size_of_array, &number_for_searching);
+    int arr[size_of_array];
+    for (int i = 0; i < size_of_array; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+ 
+    //  Linear search begins 
+    for (int i = 0; i < size_of_array ; i++)
+    {
+        if (number_for_searching == arr[i] )
+        {
+            result = i;
             break;
         }
     }
-    return -1;
-}
- 
-// Driver code
-int main(void)
-{
-
-    int arr[] = {12, 34, 72, 45, 82, 25, 91, 100, 65, 123};
-    int number_for_searching = 25;
-    int size_of_array = sizeof(arr) / sizeof(arr[0]); // finding the length of the array
-   
-    // Function call
-    int result = Linear_search(arr, size_of_array, number_for_searching);
-    (result == -1)
-        ? printf("Element is not present in array")
-        : printf("Element is present at index %d", result);
-    return 0;
+    if (result != -1)
+        printf("Element is present in the array at index %d", result);
+    else
+        printf("Element is not present in the array\n");
 }
