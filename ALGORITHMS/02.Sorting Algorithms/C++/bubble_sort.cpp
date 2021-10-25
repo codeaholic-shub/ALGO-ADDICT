06996398d332376d77e28e7fd75199fb3655f8b1
Kanav Arora <kanavarora1515@gmail.com>
2021-10-25 18:45:15 +0530

    bubble_sort.cpp file added

---
+++ ALGORITHMS/02.Sorting Algorithms/C++/bubble_sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-1-i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int swap = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = swap;
+            }
+        }
+    }
+
+    for(int ele : arr)
+    {
+        cout<<ele<<" ";
+    }
+    return 0;
+}
